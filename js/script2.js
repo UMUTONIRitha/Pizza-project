2d3189074f667e2aea4fc05585ab16c13b1311c9
UMUTONIRitha <rithamarie9@gmail.com>
2020-10-10 22:16:41 +0200

    add js for comment form

---
+++ js/script2.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#comment").submit(function(event){
+        event.preventDefault();
+        if($("input").val() ===""){
+          alert('enter a valid data')
+          return;
+        }
+          alert('your comment is received')
+        this.reset();
+      });
+    });
+      
