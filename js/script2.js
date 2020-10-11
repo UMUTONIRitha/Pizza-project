f0aadc638ab84b18e7c1a0adf0ba6f2829b9205a
UMUTONIRitha <rithamarie9@gmail.com>
2020-10-11 14:51:18 +0200

    add indentation

--- js/script2.js
+++ js/script2.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-    $("#comment").submit(function(event){
-        event.preventDefault();
-        if($("input").val() ===" "){
-          alert('enter a valid data')
-          return;
-        }
-          alert('your comment is received')
-        this.reset();
-      });
-    });
+$(document).ready(function () {
+  $("#comment").submit(function (event) {
+    event.preventDefault();
+    if ($("input").val() === " ") {
+      alert('enter a valid data')
+      return;
+    }
+    alert('your comment is received')
+    this.reset();
+  });
+});
 
-    
-      
+
